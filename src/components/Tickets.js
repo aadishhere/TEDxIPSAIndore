3fb4d07c59e31c71950cd98b016a8f1f81758cee
Aadish Jain <84015581+aadishhere@users.noreply.github.com>
2023-12-18 10:21:58 +0530

    Ticket Update

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -20,7 +20,7 @@ const Tickets = () => {
       <div className="flex w-full flex-col items-center">
         <div className="relative h-[706px] w-[360px] overflow-hidden bg-tm bg-contain bg-center lg:h-[400px]  lg:w-[1200px] lg:bg-td">
           <button
-            disabled={false}
+            disabled={true}
             className="pointer-events-none absolute bottom-[50px] right-1/2 w-[165px] translate-x-1/2 cursor-not-allowed select-none rounded-[3px] bg-grey font-semibold text-white  lg:bottom-[70px] lg:right-[125px] lg:translate-x-0 "
           >
             <GetTicketsButton/>
