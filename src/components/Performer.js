af65e2092f2de365ef26b97a81a152fb6df9f135
Aadish Jain <84015581+aadishhere@users.noreply.github.com>
2023-12-18 10:28:35 +0530

    Performance Remove

--- src/components/Performer.js
+++ src/components/Performer.js
@@ -1,12 +1,12 @@
-import Container from "@/components/utility/Container.js";
+/*import Container from "@/components/utility/Container.js";
 import RedText from "./utility/RedText";
 import SpecialPerformance from "./SpeakerCards/SpecialPerformance";
 
 const Performer = () => {
   return (
-    <Container>
+     <Container>
       <div className="flex h-fit w-full flex-col items-center justify-center gap-16 rounded-[5px] border-white py-12 pt-0 transition-all duration-200 md:h-fit md:flex-row md:gap-6 md:border md:py-6">
-        {/* AYUSH KRISHNA */}
+        {/* AYUSH KRISHNA *//*}
         <div className="h-[200px] w-full bg-ayush bg-cover bg-custom md:w-[300px] lg:bg-cover lg:bg-left"></div>
         <SpecialPerformance id="1">
           Prepare to be captivated as our mentalist{" "}
@@ -43,3 +43,6 @@ const Performer = () => {
 };
 
 export default Performer;
+
+
+*/
