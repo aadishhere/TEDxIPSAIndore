8672fa64446ea335f83026fa431bb947a38ace0d
Aadish Jain <84015581+aadishhere@users.noreply.github.com>
2023-12-18 11:01:56 +0530

    Container Box

--- src/components/LandingPage/Details.js
+++ src/components/LandingPage/Details.js
@@ -1,36 +1,29 @@
 import Link from "next/link";
 import RedText from "../utility/RedText";
 
-export default function Details() {
+export default function HeroSection() {
   return (
-    <article className="mb-24 flex w-full bg-white text-white -translate-y-[50px] flex-col items-center justify-center gap-24 text-center md:translate-y-0 lg:mb-32 lg:mt-[75px] lg:flex-row lg:gap-0">
-      {/* Section with event name */}
-      <div className="flex-1 text-[60px] font-extralight leading-[90%] lg:text-left">
-        <div>TEDx</div>
-        <div>IPSA Indore</div>
-      </div>
+    <section className="hero-section bg-gradient-to-r from-blue-500 to-purple-500 text-white py-32">
+      <div className="container mx-auto text-center">
+        <h1 className="text-5xl font-extralight mb-4">TEDx IPSA Indore</h1>
+        <p className="text-lg font-light mb-8">Inspiring Talks for a Bright Future</p>
 
-      {/* Section with event date */}
-      <div className="text-shape flex-1 select-none text-center font-extralight leading-none">
-        <div className="text-[80px]">02</div>
-        <div className="text-[30px]">MARCH '24</div>
-      </div>
+        <div className="text-shape mb-12">
+          <div className="text-4xl font-extralight">02</div>
+          <div className="text-xl">MARCH '24</div>
+        </div>
 
-      {/* Section with "See the talks HERE" */}
-      <div className="flex-1 text-[30px] font-extralight leading-none lg:text-right">
-        <div>See</div>
-        <div>the</div>
-        <div>talks</div>
-        <div>
-          {/* Using RedText utility component for styling */}
-          <RedText>
-            {/* Using Next.js Link for client-side navigation */}
-            <Link href="/talks" className="underline">
-              HERE
-            </Link>
-          </RedText>
+        <div className="text-2xl font-light">
+          <p>Explore thought-provoking talks and ideas.</p>
+          <p>
+            <RedText>
+              <Link href="/talks" className="underline">
+                Start Exploring
+              </Link>
+            </RedText>
+          </p>
         </div>
       </div>
-    </article>
+    </section>
   );
 }
