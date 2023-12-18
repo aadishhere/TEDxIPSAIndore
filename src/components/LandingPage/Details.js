d24c4dde9b27c64d26b69f44bae6d0f2e89f3f0f
Aadish Jain <84015581+aadishhere@users.noreply.github.com>
2023-12-18 11:21:02 +0530

    Container Box

--- src/components/LandingPage/Details.js
+++ src/components/LandingPage/Details.js
@@ -1,21 +1,24 @@
 import Link from "next/link";
 import RedText from "../utility/RedText";
 
-export default function Details() {
+const Details = () => {
   return (
-    <article className="mb-24 flex w-full -translate-y-[50px] flex-col items-center justify-center gap-24 text-center md:translate-y-0 lg:mb-32 lg:mt-[75px] lg:flex-row lg:gap-0 ">
-      <div className="flex-1 text-[60px] font-extralight leading-[90%] lg:text-left">
+    <article className="flex flex-col items-center justify-center gap-24 lg:flex-row lg:gap-0 mb-24 lg:mb-32">
+      {/* Event Name Section */}
+      <div className="flex-1 text-center lg:text-left text-4xl lg:text-5xl font-extralight leading-[90%]">
         <div>TEDx</div>
         <div>IPSA Indore</div>
       </div>
-      <div className="text-shape flex-1 select-none text-center font-extralight leading-none">
-        <div className="text-[80px]">02</div>
-        <div className="text-[30px]">MARCH '24</div>
+
+      {/* Event Date Section */}
+      <div className="text-shape flex-1 select-none text-center lg:text-left font-extralight">
+        <div className="text-5xl">02</div>
+        <div className="text-2xl">MARCH '24</div>
       </div>
-      <div className="flex-1 text-[30px] font-extralight leading-none lg:text-right">
-        <div>See</div>
-        <div>the</div>
-        <div>talks</div>
+
+      {/* Talks Link Section */}
+      <div className="flex-1 text-center lg:text-right text-xl font-extralight">
+        <div className="mb-4 lg:mb-0">See the talks</div>
         <div>
           <RedText>
             <Link href="/talks" className="underline">
@@ -26,4 +29,6 @@ export default function Details() {
       </div>
     </article>
   );
-}
+};
+
+export default Details;
