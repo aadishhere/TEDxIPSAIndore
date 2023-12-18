5fbe7b1323d2f6f09bc26d9ad0e3d8696af41dfd
Aadish Jain <84015581+aadishhere@users.noreply.github.com>
2023-12-18 11:18:41 +0530

    Container Box

--- src/components/LandingPage/Details.js
+++ src/components/LandingPage/Details.js
@@ -1,46 +1,29 @@
 import Link from "next/link";
 import RedText from "../utility/RedText";
 
-export default function HeroSection() {
+export default function Details() {
   return (
-    <section className="hero-section relative h-screen flex items-center justify-center overflow-hidden">
-      {/* Video Background */}
-      <video
-        autoPlay
-        loop
-        muted
-        className="absolute inset-0 w-full h-full object-cover"
-        poster="/path-to-poster-image.jpg"
-      >
-        <source src="/assets/bg.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-
-      {/* Content */}
-      <div className="text-center text-white relative z-10 w-full">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-extralight mb-4">
-          TEDx IPSA Indore
-        </h1>
-        <p className="text-lg md:text-xl lg:text-2xl font-light mb-8">
-          Inspiring Talks for a Bright Future
-        </p>
-
-        <div className="text-shape mb-12">
-          <div className="text-3xl md:text-4xl lg:text-5xl font-extralight">02</div>
-          <div className="text-lg md:text-xl">MARCH '24</div>
-        </div>
-
-        <div className="text-xl md:text-2xl font-light">
-          <p>Explore thought-provoking talks and ideas.</p>
-          <p>
-            <RedText>
-              <Link href="/talks" className="underline">
-                Start Exploring
-              </Link>
-            </RedText>
-          </p>
+    <article className="mb-24 flex w-full -translate-y-[50px] flex-col items-center justify-center gap-24 text-center md:translate-y-0 lg:mb-32 lg:mt-[75px] lg:flex-row lg:gap-0 ">
+      <div className="flex-1 text-[60px] font-extralight leading-[90%] lg:text-left">
+        <div>TEDx</div>
+        <div>IPSA Indore</div>
+      </div>
+      <div className="text-shape flex-1 select-none text-center font-extralight leading-none">
+        <div className="text-[80px]">02</div>
+        <div className="text-[30px]">MARCH '24</div>
+      </div>
+      <div className="flex-1 text-[30px] font-extralight leading-none lg:text-right">
+        <div>See</div>
+        <div>the</div>
+        <div>talks</div>
+        <div>
+          <RedText>
+            <Link href="/talks" className="underline">
+              HERE
+            </Link>
+          </RedText>
         </div>
       </div>
-    </section>
+    </article>
   );
 }
